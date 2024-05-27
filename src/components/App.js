3bb8664de9dba1823fdabf823bef6eef0d6ac1fd
AllenHo2 <aho22@dons.usfca.edu>
2024-05-26 22:32:42 -0700

    restarted

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
 import React, { Component }from "react";
-
 class App extends Component {
-    state = { artistQuery: ''};
+    state = { artistQuery: "" } //state of current parameters
 
     updateArtistQuery = event => {
-        console.log('event.target.value', event.target.value);
-        this.setState({ artistQuery: event.target.value});
+        console.log("event.target.value", event.target.value);
+        this.setState({ artistQuery : event.target.value}); //artistQuery gets updated with event value
     }
 
     searchArtist = () => {
-        console.log('this.state', this.state);
+        console.log("this.state", this.state); //shows current state of all state objects
     }
 
     handleKeyDown = event => {
-        if(event.key === 'Enter'){
+        if(event.key === "Enter") { //if event key is Enter button, then use searchArtist method
             this.searchArtist();
         }
     }
-
+    
     render() {
         return(
             <div>
-                <h2>Music Master</h2>   
-                <input 
-                 onChange={this.updateArtistQuery}
-                 onKeyDown={this.handleKeyDown}
-                 placeholder='Search for an Artist'/>
-                <button onClick={this.searchArtist}>Search</button>
+            <h2>Music Master</h2>
+            <input 
+                onChange={this.updateArtistQuery} //any changes to input calls updateArtistQuery
+                onKeyDown={this.handleKeyDown} //pressing Enter calls this method
+                placeholder="Search for Artist"/> 
+            <button onClick={this.searchArtist}>Search</button>  {/*On Click, calls search Artist method*/}
             </div>
         );
     }
